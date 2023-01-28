34e7cd689a23dfb3f41ddf0ddf9f9dcb326a1c51
savannah-yahsuanlin <savannahwang75@gmail.com>
2023-01-28 21:36:54 +0800

    add show, new, index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,27 @@
 class PostsController < ApplicationController
 	def index
+		@posts = Post.all.order('created_at ASC')
 	end
-
 
 	def new
+	
 	end
+
+	def create
+		@post = Post.new(post_params)
+		@post.save
+
+		redirect_to @post
+	end
+
+	def show
+		@post = Post.find(params[:id])
+
+		redirect_to @post
+	end
+
+	private 
+		def post_params
+			params.require(:post).permit(:title, :body)
+		end
 end

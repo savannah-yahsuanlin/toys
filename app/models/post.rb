634ccfa8ca9ab48a7f0e87667ae1e7242bafeeb1
savannah-yahsuanlin <savannahwang75@gmail.com>
2023-01-29 16:01:23 +0800

    dependent destroy

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-	has_many :comments, dependent: :destroy
+	has_many :comments, dependent: :destroy 
 
 	validates :title, presence: true
 	validates :body, presence: true
